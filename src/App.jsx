316498970cfef5f4822a3bf8401b09e128e33aa5
IvanR <ivan.ramirezf@gmail.com>
2025-01-14 00:11:55 +1000

    Dispatch ready action
    - ready action loads Question component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Body from "./Body";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
@@ -24,6 +25,12 @@ function reducer(state, action) {
       return {
         ...state,
         status: "error",
+      };
+
+    case "start":
+      return {
+        ...state,
+        status: "active",
       };
 
     default:
@@ -48,7 +55,10 @@ function App() {
       <Body>
         {status === "loading" && <Loader />}
         {status === "error" && <Error />}
-        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Body>
     </div>
   );
