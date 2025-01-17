064bca746657904b7715d9026bee05b0cb552c3a
IvanR <ivan.ramirezf@gmail.com>
2025-01-17 23:25:23 +1000

    Added FinishScreen component
    - In NextButton, if questions less than max num of questions, show Next
    button
    - In NextButton, if questions equals to max num of questions, show
    Finish button
    - Click on Finish button, changes state to 'Finished'
    - When state is 'Finished', then displays FinishScreen component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,7 @@ import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
   questions: [],
@@ -52,6 +53,9 @@ function reducer(state, action) {
 
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
+
+    case "finish":
+      return { ...state, status: "finished" };
 
     default:
       throw new Error("Unkown Action");
@@ -100,8 +104,16 @@ function App() {
               answer={answer}
             />
 
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
           </>
+        )}
+        {status === "finished" && (
+          <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} />
         )}
       </Body>
     </div>
